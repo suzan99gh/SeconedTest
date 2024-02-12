af4b51ff57b4e022a36fff9e8aadda0f6b6acb3e
suzan99gh <suzan.gh99@gmail.com>
2024-02-12 14:57:30 +0300

    3 update

--- src/mosafer/Mosafer.java
+++ src/mosafer/Mosafer.java
@@ -2,12 +2,15 @@ package mosafer;
 
 import static org.testng.Assert.assertEquals;
 
+import java.sql.Driver;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.util.Random;
 
+import org.checkerframework.checker.units.qual.Current;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -27,15 +30,15 @@ public class Mosafer extends Parameters {
 
 	}
 
-	@Test(priority = 1 )
+	@Test(priority = 1)
 	public void TestLanguageIsEnglish() {
 		String ExpectedLanaguage = "EN";
 		String ActualLanguage = driver.findElement(By.tagName("html")).getAttribute("lang").toUpperCase();
-		System.out.println("Actual Language : "+ActualLanguage);
+		System.out.println("Actual Language : " + ActualLanguage);
 		assertEquals(ActualLanguage, ExpectedLanaguage);
 	}
 
-	@Test(priority = 2)
+	@Test(priority = 2, enabled = false)
 	public void CheekCurrencySAR() {
 		String ActualCurrency = driver.findElement(By.className("sc-dRFtgE")).getText().toUpperCase();
 		System.out.println("Actual Currency : " + ActualCurrency);
@@ -43,31 +46,32 @@ public class Mosafer extends Parameters {
 		assertEquals(ActualCurrency, ExpectedCurrency);
 	}
 
-	@Test(priority = 3)
+	@Test(priority = 3, enabled = false)
 	public void CheckContactNumber() {
 		String ExpectedNumber = "+966554400000";
 		String actualNumber = driver.findElement(By.tagName("strong")).getText();
-		System.out.println("The Actual phone number : "+actualNumber);
+		System.out.println("The Actual phone number : " + actualNumber);
 
 		assertEquals(ExpectedNumber, actualNumber);
 	}
 
-	@Test(priority = 4)
+	@Test(priority = 4, enabled = false)
 	public void QetafLogo() {
 		WebElement theFooter = driver.findElement(By.tagName("footer"));
 		WebElement QetafLogo = theFooter.findElement(By.cssSelector(".sc-bdVaJa.bxRSiR.sc-ekulBa.eYboXF"));
 		assertEquals(QetafLogo.isDisplayed(), true);
 	}
-	@Test(priority = 5)
+
+	@Test(priority = 5, enabled = false)
 	public void CheckIfTheeHotelTabNotSelected() {
-		String HotelTabStatuse =driver.findElement(By.id("uncontrolled-tab-example-tab-hotels")).getAttribute("aria-selected") ;
-	System.out.println("Hotel Statuse : " + HotelTabStatuse);
-	
-	assertEquals(HotelTabStatuse, "false");
+		String HotelTabStatuse = driver.findElement(By.id("uncontrolled-tab-example-tab-hotels"))
+				.getAttribute("aria-selected");
+		System.out.println("Hotel Statuse : " + HotelTabStatuse);
+
+		assertEquals(HotelTabStatuse, "false");
 	}
-	
-	
-	@Test(priority = 6)
+
+	@Test(priority = 6, enabled = false)
 	public void CheckDepatureDateAndReturnDate() {
 
 		LocalDate today = LocalDate.now();
@@ -87,7 +91,7 @@ public class Mosafer extends Parameters {
 		assertEquals(ActualDepatureDate, expectedDepatureDate);
 
 	}
-	
+
 	@Test(priority = 7)
 	public void RandomMethodToChangeTheLanguage() {
 		Random rand = new Random();
@@ -111,5 +115,35 @@ public class Mosafer extends Parameters {
 
 		}
 
+	}
+
+	@Test(priority = 8)
+	public void SelectHotel() {
+		driver.findElement(By.id("uncontrolled-tab-example-tab-hotels")).click();
+		if (driver.getCurrentUrl().contains("ar")) {
+			driver.findElement(By.className("phbroq-2")).sendKeys(CitiesInArabic[randomArabicCity]);
+		} else {
+			driver.findElement(By.className("phbroq-2")).sendKeys(CitiesInEnglish[randomEnglishCity]);
+		}
+		WebElement theList = driver.findElement(By.className("UzzIN"));
+
+		theList.findElements(By.tagName("li")).get(1).click();
+	}
+
+	@Test(priority = 9)
+	public void select() {
+		WebElement findSelect =driver.findElement(By.className("tln3e3-1"));
+		Select selector = new Select(findSelect);
+		System.out.println( "the random index for option : " +randomOption);
+		selector.selectByIndex(randomOption);
+		
+		driver.findElement(By.cssSelector(".sc-jTzLTM.hQpNle.sc-1vkdpp9-6.iKBWgG.js-HotelSearchBox__SearchButton.btn.btn-primary.btn-block")).click();;
+	
+	}
+	
+	@Test
+	private void pub() {
+//		وجدنا 
+//		found 
 	}
 }
--- src/mosafer/Parameters.java
+++ src/mosafer/Parameters.java
@@ -5,21 +5,23 @@ import java.util.Random;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Parameters  {
-		 
-		 WebDriver driver = new ChromeDriver();
-	String URL = "https://global.almosafer.com/en";
-	
+public class Parameters {
 
-	Random randForcity = new Random();
+	WebDriver driver = new ChromeDriver();
+	String URL = "https://global.almosafer.com/en";
+
+	Random rand = new Random();
 
 	String[] Websites = { "https://global.almosafer.com/en", "https://global.almosafer.com/ar" };
 
 	String[] CitiesInEnglish = { "dubai", "jeddah", "riyadh" };
 
-	int randomEnglishCity = randForcity.nextInt(CitiesInEnglish.length);
+	int randomEnglishCity = rand.nextInt(CitiesInEnglish.length);
 
 	String[] CitiesInArabic = { "دبي", "جدة" };
 
-	int randomArabicCity = randForcity.nextInt(CitiesInArabic.length);
+	int randomArabicCity = rand.nextInt(CitiesInArabic.length);
+
+	int randomOption = rand.nextInt(2);
+
 }
